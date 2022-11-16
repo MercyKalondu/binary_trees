f00940954b9dba4fd20de66e90b2c626054de712
MercyKalondu <mercykalondu62@gmail.com>
2022-11-16 10:38:23 -0800

    uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -16,3 +16,17 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	return (node->parent->right);
 }
+
+/**
+ * binary_tree_uncle - find the uncle of a node
+ *
+ * @node: node
+ * Return: pointer to the uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
